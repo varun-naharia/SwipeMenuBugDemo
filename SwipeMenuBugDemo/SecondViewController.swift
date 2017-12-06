5a8a8b6cc42edf008cabfb403ff0e2997fa12118
Varun Naharia <v.naharia@logictrixtech.com>
2017-12-06 12:30:42 +0530

    initial commit

---
+++ SwipeMenuBugDemo/SecondViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SecondViewController.swift
+//  SwipeMenuBugDemo
+//
+//  Created by Varun Naharia on 06/12/17.
+//  Copyright © 2017 Varun Naharia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+   
+    
+    @IBAction func printAction(_ sender: UIButton) {
+        print("SecondViewController")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SwipeMenuBugDemo/ViewController.swift
+++ SwipeMenuBugDemo/ViewController.swift
@@ -8,16 +8,92 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: SwipeMenuViewController  {
 
+    private var datas: [String] = ["First","Second", "Third"]
+    
+    var options = SwipeMenuViewOptions()
+    var dataCount: Int = 3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        options.tabView.style = .segmented
+        options.tabView.margin                          = 8.0
+        options.tabView.underlineView.backgroundColor   = UIColor.white
+        options.tabView.backgroundColor                 = UIColor(red: 0.18, green: 0.18, blue: 0.18, alpha: 1.00)
+        options.tabView.underlineView.height            = 3.0
+        options.tabView.itemView.textColor              = UIColor(red: 0.60, green: 0.60, blue: 0.60, alpha: 1.00)
+        options.tabView.itemView.selectedTextColor      = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+        options.tabView.itemView.margin                 = 10.0
+        options.contentScrollView.backgroundColor       = UIColor.lightGray
+        options.tabView.isSafeAreaEnabled = true
+        options.tabView.height = 60
+        self.reload()
+        // Do any additional setup after loading the view.
     }
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveAction(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func reload() {
+        swipeMenuView.reloadData(options: options)
+    }
+    
+    // MARK: - SwipeMenuViewDelegate
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewWillSetupAt currentIndex: Int) {
+        print("will setup SwipeMenuView")
+    }
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewDidSetupAt currentIndex: Int) {
+        print("did setup SwipeMenuView")
+    }
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, willChangeIndexFrom fromIndex: Int, to toIndex: Int) {
+        print("will change from section\(fromIndex + 1)  to section\(toIndex + 1)")
+    }
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, didChangeIndexFrom fromIndex: Int, to toIndex: Int) {
+        print("did change from section\(fromIndex + 1)  to section\(toIndex + 1)")
+    }
+    
+    
+    // MARK - SwipeMenuViewDataSource
+    
+    override func numberOfPages(in swipeMenuView: SwipeMenuView) -> Int {
+        return dataCount
+    }
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, titleForPageAt index: Int) -> String {
+        return datas[index]
+    }
+    
+    override func swipeMenuView(_ swipeMenuView: SwipeMenuView, viewControllerForPageAt index: Int) -> UIViewController {
+        
+        switch index {
+        case 0:
+            let vc:FirstViewController = self.storyboard?.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
+            return vc
+        case 1:
+            let vc:SecondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+            return vc
+        case 2:
+            let vc:ThirdViewController = self.storyboard?.instantiateViewController(withIdentifier: "ThirdViewController") as! ThirdViewController
+            return vc
+        default:
+            let vc:FirstViewController = self.storyboard?.instantiateViewController(withIdentifier: "FirstViewController") as! FirstViewController
+            return vc
+        }
     }
 
 
